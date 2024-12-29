b6a5533b57dd14d9a40b71bec1e03ccf9a6f3fdf
dd2012 <demondark20@gmail.com>
2024-12-29 04:20:57 +0300

    change module dep

--- Source/UnrealSandboxTerrain/UnrealSandboxTerrain.Build.cs
+++ Source/UnrealSandboxTerrain/UnrealSandboxTerrain.Build.cs
@@ -2,62 +2,14 @@
 
 using UnrealBuildTool;
 
-public class UnrealSandboxTerrain : ModuleRules
-{
-	public UnrealSandboxTerrain(ReadOnlyTargetRules Target) : base(Target)
-	{
+public class UnrealSandboxTerrain : ModuleRules {
+	public UnrealSandboxTerrain(ReadOnlyTargetRules Target) : base(Target) {
 
 		//PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
         CppStandard = CppStandardVersion.Cpp20;
-        
-        PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				"RenderCore",
-				"RHI",
-				"Json",
-				"JsonUtilities",
-				"Networking",
-				"Sockets",
 
-				
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
-	}
+		PublicDependencyModuleNames.AddRange(new string[] { "Core" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine", "Slate", "SlateCore", "RenderCore", "RHI" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "Json", "JsonUtilities", "Networking", "Sockets", "UnrealSandboxKvDb" });
+    }
 }
